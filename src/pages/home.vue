10fbccfa68777e4677dc4be867dee12d44d76822
liqiantu <liqiantu@jammyfm.com>
2020-12-12 15:25:57 +0800

    调整首页布局

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,59 +1,52 @@
 <template>
-  <a-layout id="components-layout-demo-top">
-    <a-layout-header>
-      <div class="logo"> 阅读 </div>
-      <a-menu
-        theme="dark"
-        mode="horizontal"
-        :default-selected-keys="['2']"
-        :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="2" @click="onMenuClick">
-          期刊
-        </a-menu-item>
-        <a-menu-item key="3" @click="onMenuClick">
-          图书
-        </a-menu-item>
-        <a-menu-item key="7" @click="onMenuClick">
-          报纸
-        </a-menu-item>
+  <a-layout id="components-layout-demo-fixed-sider">
+    <a-layout-sider
+      :style="{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }"
+      v-model="collapsed"
+      collapsible
+    >
+      <div class="logo" />
+      <a-menu theme="dark" mode="inline" :default-selected-keys="['2']">
+        <a-menu-item key="2" @click="onMenuClick"> 期刊 </a-menu-item>
+        <a-menu-item key="3" @click="onMenuClick"> 图书 </a-menu-item>
+        <a-menu-item key="7" @click="onMenuClick"> 报纸 </a-menu-item>
       </a-menu>
-    </a-layout-header>
+    </a-layout-sider>
 
-    <a-layout-content>
-       <router-view></router-view>
-    </a-layout-content>
-
-    <a-layout-footer style="text-align: center">
-      liqiantu ©2020 Created by Ant UED
-    </a-layout-footer>
-
+    <a-layout :style="{ marginLeft: '200px' }">
+      <a-layout-header :style="{ background: '#fff', padding: 0 }" />
+      <a-layout-content :style="{ margin: '24px 16px 0', overflow: 'initial' }">
+        <router-view></router-view>
+      </a-layout-content>
+      <a-layout-footer :style="{ textAlign: 'center' }">
+        Liqiantu ©2020 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
   </a-layout>
-
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
   methods: {
     onMenuClick(item) {
       if (item.item.isSelected) return;
-      console.log('key', item);
+      console.log("key", item);
       let url = `/index?kind=${item.key}`;
       this.$router.push(url);
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
-#components-layout-demo-top .logo {
-  line-height: 31px;
-  text-align: center;
-  color: white;
-  width: 120px;
-  height: 31px;
+#components-layout-demo-fixed-sider .logo {
+  height: 32px;
   background: rgba(255, 255, 255, 0.2);
-  margin: 16px 24px 16px 0;
-  float: left;
+  margin: 16px;
 }
 </style>
